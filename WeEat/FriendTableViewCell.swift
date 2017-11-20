0c5cc13b4f962f167d3ab4a714f3e31c6615b27c
neycwang <neycwang@gmail.com>
2017-11-20 14:49:32 +0800

    AddFriend

---
+++ WeEat/FriendTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendTableViewCell.swift
+//  WeEat
+//
+//  Created by apple on 2017/11/20.
+//  Copyright © 2017年 Wang. All rights reserved.
+//
+
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
